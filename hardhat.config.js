1b95b05659ed58d4bd330e9df881007d378412ed
KuantumKnight <msarvesh.dav@gmail.com>
2026-08-20 20:43:00 +0530

    Optimize contract bytecode for deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@ const sepoliaRpcUrl = process.env.SEPOLIA_RPC_URL;
 const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY;
 
 export default {
-  solidity: { version: "0.8.26", settings: { evmVersion: "cancun" } },
+    solidity: { version: "0.8.26", settings: { evmVersion: "cancun", optimizer: { enabled: true, runs: 1 } } },
   networks: {
     hardhat: {},
     localhost: { url: "http://127.0.0.1:8545" },
